83f4011aa4427a1b211596b6d4f7256737904af2
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-06-02 11:57:51 -0300

    Added check_apt.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'MIT'
 description      'Installs/Configures mo_monitoring_client'
 long_description 'Installs/Configures mo_monitoring_client'
 
-version          '1.0.9'
+version          '1.0.10'
 
 depends 'nrpe', '~> 1.5.0'
