5a500849ea6576d634d49df2a995ebdde699051f
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-10-26 14:41:33 -0300

    Added DNS monitoring recipe.

--- attributes/default.rb
+++ attributes/default.rb
@@ -43,3 +43,5 @@ default['mo_monitoring_client']['plugins']['check_io_queue']['parameters']      
 default['mo_monitoring_client']['plugins']['check_io_waiting_time']['warning_condition']   = 250
 default['mo_monitoring_client']['plugins']['check_io_waiting_time']['critical_condition']  = 350
 default['mo_monitoring_client']['plugins']['check_io_waiting_time']['parameters']          = "-W -d sda"
+
+default['mo_monitoring_client']['plugins']['check_dns']['parameters']                      = "-c example.com a 127.0.0.1"
--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ license          'MIT'
 description      'Installs/Configures mo_monitoring_client'
 long_description 'Installs/Configures mo_monitoring_client'
 
-version          '1.0.15'
+version          '1.0.16'
 
 depends 'nrpe', '~> 1.5.0'
